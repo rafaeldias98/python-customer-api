1aad39df271500436c7688de868bd8a2a28b6796
Rafael Dias <rafadias-98@hotmail.com>
2019-10-24 00:30:20 -0300

    Add  coverage command on test script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -3,6 +3,11 @@ run() {
     docker-compose exec api python3 manage.py test
 }
 
+coverage() {
+    docker-compose exec api coverage run --source='./api' --omit="*/tests*,*/migrations*,*init*" manage.py test
+    docker-compose exec api coverage html
+}
+
 help() {
     echo 'Available subcommands: run'
 }
