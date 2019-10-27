6e0d76e66bc2c58faca7f6cad7718111e680a915
Rafael Dias <rafadias-98@hotmail.com>
2019-10-27 13:15:55 -0300

    Adding coverage inline report and coverage command in help description

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -5,11 +5,12 @@ run() {
 
 coverage() {
     docker-compose exec api coverage run --source='./api' --omit="*/tests*,*/migrations*,*init*" manage.py test
+    docker-compose exec api coverage report
     docker-compose exec api coverage html
 }
 
 help() {
-    echo 'Available subcommands: run'
+    echo 'Available subcommands: run, coverage'
 }
 
 [[ -z $@ ]] && (help ; exit 1)
